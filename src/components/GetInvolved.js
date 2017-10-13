c73dc779ec1d7fa32c251f20795f0427af88c5b7
Andrew Mager <mager@postmates.com>
2017-10-13 11:23:49 -0400

    todo

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -5,6 +5,7 @@ class GetInvolved extends Component {
     return (
       <section className="section">
         <h3 className="title">Get Involved</h3>
+        {/* TODO: https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md */}
       </section>
     );
   }
