405ae2b7a12bf3575de9ab9b6516893d62857fe8
Andrew Mager <mager@postmates.com>
2017-10-16 10:57:20 -0400

    Start responsiveness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar" aria-label="main navigation">
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            <a className="navbar-item">Get Involved</a>
-            <a className="navbar-item">Community</a>
-            <a className="navbar-item">About</a>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav className="navbar" aria-label="main navigation">
+                <div className="navbar-menu is-active">
+                    <div className="navbar-end">
+                        <a className="navbar-item">Get Involved</a>
+                        <a className="navbar-item">Community</a>
+                        <a className="navbar-item">About</a>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default NavBar;
