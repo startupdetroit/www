0173f30b262393b609db15e39a343b3aba685d6a
Andrew Mager <mager@postmates.com>
2017-10-16 12:03:57 -0400

    Slack button, calendar

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 class GetInvolved extends Component {
-  render() {
-    return (
-      <section className="section">
-        <h3 className="title">Get Involved</h3>
-        {/* TODO: https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md */}
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section className="GetInvolved section">
+                <h3 className="title">Get Involved</h3>
+                <div className="buttons">
+                    <a
+                        href="https://startupdetroit.herokuapp.com/"
+                        className="button is-link is-outlined is-large"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Join our Slack community!
+                    </a>
+                    <a
+                        href="https://www.startupdigest.com/digests/detroit/"
+                        className="button is-link is-outlined is-large"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Startup Digest Calendar
+                    </a>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default GetInvolved;
