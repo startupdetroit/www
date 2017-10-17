92b3f53e1e1964945b4dfcee2754e4f135ccb4c6
Andrew Mager <mager@postmates.com>
2017-10-17 12:02:24 -0400

    Four more logos

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Logo from './Logo';
 
+import logoAmbassador from '../static/img/logos/logo-ambassador.png';
+import logoBamboo from '../static/img/logos/logo-bamboo.png';
 import logoDVP from '../static/img/logos/logo-dvp.png';
 import logoMapbox from '../static/img/logos/logo-mapbox.png';
 import logoMVCA from '../static/img/logos/logo-mvca.png';
+import logoSkyspecs from '../static/img/logos/logo-skyspecs.png';
 import logoTechstars from '../static/img/logos/logo-techstars.png';
+import logoTrucks from '../static/img/logos/logo-trucks.png';
 
 class Community extends Component {
   render() {
@@ -12,6 +16,20 @@ class Community extends Component {
       <section className="section" id="community">
         <h3 className="title">Community</h3>
         <div className="logos">
+          <Logo
+            src={logoAmbassador}
+            location="Birmingham, MI"
+            href="http://getambassador.com"
+            name="Ambassador"
+            type="Startup"
+          />
+          <Logo
+            src={logoBamboo}
+            location="Detroit, MI"
+            href="http://bamboodetroit.com"
+            name="Bamboo Detroit"
+            type="Startup"
+          />
           <Logo
             src={logoDVP}
             location="Detroit, MI"
@@ -34,10 +52,24 @@ class Community extends Component {
             type="Community Supporter"
           />
           <Logo
+            src={logoSkyspecs}
+            location="Detroit, MI"
+            href="http://www.skyspecs.com"
+            name="Skyspecs"
+            type="Startup"
+          />
+          <Logo
             src={logoTechstars}
             location="Detroit, MI"
             href="http://techstars.com"
             name="Techstars"
+            type="Investor"
+          />
+          <Logo
+            src={logoTrucks}
+            location="Detroit, MI"
+            href="http://trucks.vc"
+            name="Trucks Venture Capital"
             type="Investor"
           />
         </div>
