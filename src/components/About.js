db03e51301ac381fb322454d8cd0509675af9457
Ted Serbinski <tedserbinski@users.noreply.github.com>
2017-10-18 09:21:54 -0400

    quick update to about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,11 @@ class About extends Component {
     return (
       <section className="section" id="about">
         <h3 className="title">About</h3>
+        <p>#StartupDetroit is an open source approach to building the greater Detroit Startup community.</p>
+        <p>Communities thrive when the are they open and inclusive. Open source is an excellent model to make this work.</p>
+        <p>Our goal of #StartupDetroit is to create a brand owned by the community, not owned by anyone one organization.</p>
+        <p>To build an inclusive community, we want startups, investors, and supporting organziations and companies to be invited to participate. We do not want to limit participation by location. Rather, #StartupDetroit is for the startups, investors, and supporting organizations in Detroit, around metro Detroit, and around the world that want to see this region thrive.</p>
+        <p>Thank you for your support!</p>
       </section>
     );
   }
