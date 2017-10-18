b0406f899b422086c732e8bda2ba57cd38b4fa96
Andrew Mager <mager@postmates.com>
2017-10-18 09:44:57 -0400

    Format About

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,34 @@ class About extends Component {
     return (
       <section className="section" id="about">
         <h3 className="title">About</h3>
-        <p>#StartupDetroit is an open source approach to building the Detroit Startup community.</p>
-        <p>Startup Detroit is for the makers, creators, investors, and supporters in the software and tech startup space building and investing in innovation in and around metro Detroit.</p>
-        <p>Communities thrive when the are they open and inclusive. Open source is an excellent model to make this work.</p>
-        <p>Our goal of #StartupDetroit is to create a brand owned by the community, not owned by anyone one organization.</p>
-        <p>To build an inclusive community, we want startups, investors, and supporting organziations and companies to be invited to participate. We do not want to limit participation by location. Rather, #StartupDetroit is for the startups, investors, and supporting organizations in Detroit, around metro Detroit, and around the world that want to see this region thrive.</p>
-        <p>Thank you for your support!</p>
+        <div className="content">
+          <p>
+            #StartupDetroit is an open source approach to building the Detroit
+            Startup community.
+          </p>
+          <p>
+            Startup Detroit is for the makers, creators, investors, and
+            supporters in the software and tech startup space building and
+            investing in innovation in and around metro Detroit.
+          </p>
+          <p>
+            Communities thrive when the are they open and inclusive. Open source
+            is an excellent model to make this work.
+          </p>
+          <p>
+            Our goal of #StartupDetroit is to create a brand owned by the
+            community, not owned by anyone one organization.
+          </p>
+          <p>
+            To build an inclusive community, we want startups, investors, and
+            supporting organziations and companies to be invited to participate.
+            We do not want to limit participation by location. Rather,
+            #StartupDetroit is for the startups, investors, and supporting
+            organizations in Detroit, around metro Detroit, and around the world
+            that want to see this region thrive.
+          </p>
+          <p>Thank you for your support!</p>
+        </div>
       </section>
     );
   }
