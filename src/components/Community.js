6bba36a1e6ac8853ff81527b37df466a15e2723f
Shriyash Jalukar <shriyash1111@gmail.com>
2017-10-18 08:29:39 -0400

    Add Tome Software, reorder alphabetically

--- src/components/Community.js
+++ src/components/Community.js
@@ -3,24 +3,25 @@ import Logo from './Logo';
 
 import logoAmbassador from '../static/img/logos/logo-ambassador.png';
 import logoBamboo from '../static/img/logos/logo-bamboo.png';
+import logoCastle from '../static/img/logos/logo-castle.png';
+import logoDistilNetworks from '../static/img/logos/logo-distilnetworks.png';
+import logoDuo from '../static/img/logos/logo-duo.png';
 import logoDVP from '../static/img/logos/logo-dvp.png';
+import logoRepurpose from '../static/img/logos/logo-repurpose.jpg';
 import logoLunar from '../static/img/logos/logo-lunar.png';
+import logoMapbox from '../static/img/logos/logo-mapbox.png';
+import logoMVCA from '../static/img/logos/logo-mvca.png';
+import logoMySwimPro from '../static/img/logos/logo-myswimpro.png';
+import logoSkyspecs from '../static/img/logos/logo-skyspecs.png';
+import logoSpotHero from '../static/img/logos/logo-spothero.png';
 import logoStockX from '../static/img/logos/logo-stockx.jpg';
 import logoTechstars from '../static/img/logos/logo-techstars.png';
 import logoTechtown from '../static/img/logos/logo-techtown.png';
-import logoRepurpose from '../static/img/logos/logo-repurpose.jpg';
-import logoWaymark from '../static/img/logos/logo-waymark.png';
-import logoVFA from '../static/img/logos/logo-vfa.png';
-import logoDuo from '../static/img/logos/logo-duo.png';
-import logoMapbox from '../static/img/logos/logo-mapbox.png';
-import logoMySwimPro from '../static/img/logos/logo-myswimpro.png';
-import logoSkyspecs from '../static/img/logos/logo-skyspecs.png';
-import logoDistilNetworks from '../static/img/logos/logo-distilnetworks.png';
-import logoMVCA from '../static/img/logos/logo-mvca.png';
-import logoSpotHero from '../static/img/logos/logo-spothero.png';
+import logoTome from '../static/img/logos/logo-tome.jpg';
 import logoTrucks from '../static/img/logos/logo-trucks.png';
+import logoVFA from '../static/img/logos/logo-vfa.png';
+import logoWaymark from '../static/img/logos/logo-waymark.png';
 import logoWSU from '../static/img/logos/logo-wsu.png';
-import logoCastle from '../static/img/logos/logo-castle.png';
 
 
 class Community extends Component {
@@ -44,6 +45,27 @@ class Community extends Component {
             type="Startup"
           />
           <Logo
+            src={logoCastle}
+            location="Detroit, MI"
+            href="https://entercastle.com/"
+            name="Castle"
+            type="Startup"
+          />
+          <Logo
+            src={logoDistilNetworks}
+            location="San Francisco, CA"
+            href="http://distillnetworks.com"
+            name="Distil Networks"
+            type="Community Supporter"
+          />
+          <Logo
+            src={logoDuo}
+            location="Ann Arbor, MI"
+            href="https://duo.com/"
+            name="Duo Security"
+            type="Startup"
+          />
+          <Logo
             src={logoDVP}
             location="Detroit, MI"
             href="http://detroit.vc"
@@ -51,11 +73,53 @@ class Community extends Component {
             type="Investor"
           />
           <Logo
+            src={logoRepurpose}
+            location="Detroit, MI"
+            href="http://www.repurpose.co"
+            name="re:purpose"
+            type="Startup"
+          />
+          <Logo
             src={logoLunar}
             location="Detroit, MI"
             href="https://lunarwireless.com/"
             name="Lunar Wireless"
             type="Startup"
+          />
+          <Logo
+            src={logoMapbox}
+            location="San Francisco, CA"
+            href="http://mapbox.com"
+            name="Mapbox"
+            type="Community Supporter"
+          />
+          <Logo
+            src={logoMVCA}
+            location="Ann Arbor, MI"
+            href="http://michiganvca.org"
+            name="Michigan Venture Capital Association"
+            type="Investor"
+          />
+          <Logo
+            src={logoMySwimPro}
+            location="Detroit, MI"
+            href="http://myswimpro.com"
+            name="MySwimPro"
+            type="Startup"
+          />
+          <Logo
+            src={logoSkyspecs}
+            location="Ann Arbor, MI"
+            href="http://www.skyspecs.com"
+            name="Skyspecs"
+            type="Startup"
+          />
+          <Logo
+            src={logoSpotHero}
+            location="Detroit, MI"
+            href="http://www.spothero.com"
+            name="Spot Hero"
+            type="Community Supporter"
           />
           <Logo
             src={logoStockX}
@@ -79,74 +143,11 @@ class Community extends Component {
             type="Community Supporter"
           />
           <Logo
-            src={logoRepurpose}
-            location="Detroit, MI"
-            href="http://www.repurpose.co"
-            name="re:purpose"
+            src={logoTome}
+            location="Royal Oak, MI"
+            href="http://tomesoftware.com/"
+            name="Tome Software"
             type="Startup"
-          />
-          <Logo
-            src={logoWaymark}
-            location="Detroit, MI"
-            href="http://waymark.com/"
-            name="Waymark"
-            type="Startup"
-          />
-          <Logo
-            src={logoVFA}
-            location="Detroit, MI"
-            href="https://ventureforamerica.org/"
-            name="Venture for America"
-            type="Community Supporter"
-          />
-          <Logo
-            src={logoDuo}
-            location="Ann Arbor, MI"
-            href="https://duo.com/"
-            name="Duo Security"
-            type="Startup"
-          />
-          <Logo
-            src={logoMapbox}
-            location="San Francisco, CA"
-            href="http://mapbox.com"
-            name="Mapbox"
-            type="Community Supporter"
-          />
-          <Logo
-            src={logoMySwimPro}
-            location="Detroit, MI"
-            href="http://myswimpro.com"
-            name="MySwimPro"
-            type="Startup"
-          />
-          <Logo
-            src={logoSkyspecs}
-            location="Ann Arbor, MI"
-            href="http://www.skyspecs.com"
-            name="Skyspecs"
-            type="Startup"
-          />
-          <Logo
-            src={logoDistilNetworks}
-            location="San Francisco, CA"
-            href="http://distillnetworks.com"
-            name="Distil Networks"
-            type="Community Supporter"
-          />
-          <Logo
-            src={logoMVCA}
-            location="Ann Arbor, MI"
-            href="http://michiganvca.org"
-            name="Michigan Venture Capital Association"
-            type="Investor"
-          />
-          <Logo
-            src={logoSpotHero}
-            location="Detroit, MI"
-            href="http://www.spothero.com"
-            name="Spot Hero"
-            type="Community Supporter"
           />
           <Logo
             src={logoTrucks}
@@ -156,18 +157,25 @@ class Community extends Component {
             type="Investor"
           />
           <Logo
+            src={logoVFA}
+            location="Detroit, MI"
+            href="https://ventureforamerica.org/"
+            name="Venture for America"
+            type="Community Supporter"
+          />
+          <Logo
+            src={logoWaymark}
+            location="Detroit, MI"
+            href="http://waymark.com/"
+            name="Waymark"
+            type="Startup"
+          />
+          <Logo
             src={logoWSU}
             location="Detroit, MI"
             href="http://innovationwarriors.wayne.edu/"
             name="WSU Innovation Warriors"
             type="Community Supporter"
-          />
-          <Logo
-            src={logoCastle}
-            location="Detroit, MI"
-            href="https://entercastle.com/"
-            name="Castle"
-            type="Startup"
           />
         </div>
       </section>
