c418fbd16238620d773ab94c8e68eacd8cf87b29
Eric Krawczyk <eric.krawczyk@nokia.com>
2018-07-30 15:58:21 -0400

    Refactor rendering organizations for modularity

---
+++ src/constants/organizations.js
@@ -0,0 +1,284 @@
+import logoAmbassador from '../static/img/logos/logo-ambassador.png';
+import logoAssemblerLabs from '../static/img/logos/logo-assembler-labs.png';
+import logoAutobooks from '../static/img/logos/logo-autobooks.png';
+import logoBamboo from '../static/img/logos/logo-bamboo.png';
+import logoBenzinga from '../static/img/logos/logo-benzinga.png';
+import logoBloomscape from '../static/img/logos/logo-bloomscape.png';
+import logoCastle from '../static/img/logos/logo-castle.png';
+import logoDistilNetworks from '../static/img/logos/logo-distilnetworks.png';
+import logoDuo from '../static/img/logos/logo-duo.png';
+import logoDVP from '../static/img/logos/logo-dvp.png';
+import logoGenomenon from '../static/img/logos/logo-genomenon.png';
+import logoGrandCircus from '../static/img/logos/logo-grandcircus.png';
+import logoGrandVentures from '../static/img/logos/logo-grandventures.png';
+import logoLunar from '../static/img/logos/logo-lunar.png';
+import logoMapbox from '../static/img/logos/logo-mapbox.png';
+import logoMightyAI from '../static/img/logos/logo-mightyai.svg';
+import logoMVCA from '../static/img/logos/logo-mvca.png';
+import logoMySwimPro from '../static/img/logos/logo-myswimpro.png';
+import logoOptimize from '../static/img/logos/logo-optimize.png';
+import logoPlanetM from '../static/img/logos/logo-planetm.jpg';
+import logoRepurpose from '../static/img/logos/logo-repurpose.jpg';
+import logoSift from '../static/img/logos/logo-sift.png';
+import logoSkillo from '../static/img/logos/logo-skillo.png';
+import logoSkyspecs from '../static/img/logos/logo-skyspecs.png';
+import logoSPLT from '../static/img/logos/logo-splt.png';
+import logoSpotHero from '../static/img/logos/logo-spothero.png';
+import logoStockX from '../static/img/logos/logo-stockx.jpg';
+import logoTechstars from '../static/img/logos/logo-techstars.png';
+import logoTechtown from '../static/img/logos/logo-techtown.png';
+import logoTome from '../static/img/logos/logo-tome.jpg';
+import logoTrucks from '../static/img/logos/logo-trucks.png';
+import logoVFA from '../static/img/logos/logo-vfa.png';
+import logoWaymark from '../static/img/logos/logo-waymark.png';
+import logoWeWork from '../static/img/logos/logo-wework.jpg';
+import logoWSU from '../static/img/logos/logo-wsu.png';
+
+import {ORG_TYPES} from './organization-types.js';
+
+export const ORGANIZATIONS = [{
+    logo: logoAmbassador,
+    location: "Royal Oak, MI",
+    website: "http://getambassador.com",
+    name: "Ambassador",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoAssemblerLabs,
+    location: "Detroit, MI",
+    website: "https://www.assemblerlabs.com",
+    name: "Assembler Labs",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoAutobooks,
+    location: "Detroit, MI",
+    website: "http://autobooks.co",
+    name: "Autobooks",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoBamboo,
+    location: "Detroit, MI",
+    website: "http://bamboodetroit.com",
+    name: "Bamboo Detroit",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoBenzinga,
+    location: "Detroit, MI",
+    website: "https://www.benzinga.com/",
+    name: "Benzinga",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoBloomscape,
+    location: "Detroit, MI",
+    website: "https://www.bloomscape.com/",
+    name: "Bloomscape",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoCastle,
+    location: "Detroit, MI",
+    website: "https://entercastle.com/",
+    name: "Castle",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoDistilNetworks,
+    location: "San Francisco, CA",
+    website: "http://distillnetworks.com",
+    name: "Distil Networks",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoDuo,
+    location: "Ann Arbor, MI",
+    website: "https://duo.com/",
+    name: "Duo Security",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoDVP,
+    location: "Detroit, MI",
+    website: "http://detroit.vc",
+    name: "Detroit Venture Partners",
+    type: ORG_TYPES.INVESTOR,
+  },
+  {
+    logo: logoGenomenon,
+    location: "Ann Arbor, MI",
+    website: "https://www.genomenon.com/",
+    name: "Genomenon",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoGrandCircus,
+    location: "Detroit, MI",
+    website: "https://www.grandcircus.co/",
+    name: "Grand Circus",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoGrandVentures,
+    location: "Grand Rapids, MI",
+    website: "http://grandvcp.com/",
+    name: "Grand Ventures",
+    type: ORG_TYPES.INVESTOR,
+  },
+  {
+    logo: logoLunar,
+    location: "Detroit, MI",
+    website: "https://lunarwireless.com/",
+    name: "Lunar Wireless",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoMapbox,
+    location: "San Francisco, CA",
+    website: "http://mapbox.com",
+    name: "Mapbox",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoMightyAI,
+    location: "San Francisco, CA",
+    website: "https://mty.ai/",
+    name: "Mighty AI",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoMVCA,
+    location: "Ann Arbor, MI",
+    website: "http://michiganvca.org",
+    name: "Michigan Venture Capital Association",
+    type: ORG_TYPES.INVESTOR,
+  },
+  {
+    logo: logoMySwimPro,
+    location: "Detroit, MI",
+    website: "http://myswimpro.com",
+    name: "MySwimPro",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoOptimize,
+    location: "Detroit, MI",
+    website: "https://www.optimizemi.org/",
+    name: "Optimize",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoPlanetM,
+    location: "Detroit, MI",
+    website: "https://planetm.michiganbusiness.org/",
+    name: "PlanetM",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoRepurpose,
+    location: "Detroit, MI",
+    website: "http://www.repurpose.co",
+    name: "re:purpose",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoSift,
+    location: "Detroit, MI",
+    website: "http://www.justsift.com",
+    name: "Sift",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoSkillo,
+    location: "Detroit, MI",
+    website: "http://tryskillo.com/",
+    name: "Skillo",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoSkyspecs,
+    location: "Ann Arbor, MI",
+    website: "http://www.skyspecs.com",
+    name: "Skyspecs",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoSPLT,
+    location: "Detroit, MI",
+    website: "http://www.splt.io",
+    name: "SPLT",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoSpotHero,
+    location: "Detroit, MI",
+    website: "http://www.spothero.com",
+    name: "Spot Hero",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoStockX,
+    location: "Detroit, MI",
+    website: "http://www.stockx.com",
+    name: "StockX",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoTechstars,
+    location: "Detroit, MI",
+    website: "http://techstars.com",
+    name: "Techstars",
+    type: ORG_TYPES.INVESTOR,
+  },
+  {
+    logo: logoTechtown,
+    location: "Detroit, MI",
+    website: "http://techtown.com",
+    name: "Techtown",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoTome,
+    location: "Royal Oak, MI",
+    website: "http://tomesoftware.com/",
+    name: "Tome Software",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoTrucks,
+    location: "San Francisco, CA",
+    website: "http://trucks.vc",
+    name: "Trucks Venture Capital",
+    type: ORG_TYPES.INVESTOR,
+  },
+  {
+    logo: logoVFA,
+    location: "Detroit, MI",
+    website: "https://ventureforamerica.org/",
+    name: "Venture for America",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+  {
+    logo: logoWaymark,
+    location: "Detroit, MI",
+    website: "http://waymark.com/",
+    name: "Waymark",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoWeWork,
+    location: "Detroit, MI",
+    website: "http://wework.com/",
+    name: "Wework",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoWSU,
+    location: "Detroit, MI",
+    website: "http://innovationwarriors.wayne.edu/",
+    name: "WSU Innovation Warriors",
+    type: ORG_TYPES.COMMUNITY_SUPPORTER,
+  },
+]
