ad99b04c05b8307b306f8d5f4d8ba0780ce50a4d
Jakob Rodseth <jrodseth12@gmail.com>
2019-06-10 13:16:37 -0400

    Add Neon logo

--- src/constants/organizations.js
+++ src/constants/organizations.js
@@ -24,6 +24,7 @@ import logoMercuryFund from '../static/img/logos/logo-mercuryfund.png';
 import logoMightyAI from '../static/img/logos/logo-mightyai.svg';
 import logoMVCA from '../static/img/logos/logo-mvca.png';
 import logoMySwimPro from '../static/img/logos/logo-myswimpro.png';
+import logoNeon from '../static/img/logos/logo-neon.png'
 import logoNextEnergy from '../static/img/logos/logo-nextenergy.png';
 import logoOptimize from '../static/img/logos/logo-optimize.png';
 import logoPlanetM from '../static/img/logos/logo-planetm.jpg';
@@ -233,6 +234,13 @@ export const ORGANIZATIONS = [{
     location: "Detroit, MI",
     website: "http://myswimpro.com",
     name: "MySwimPro",
+    type: ORG_TYPES.STARTUP,
+  },
+  {
+    logo: logoNeon,
+    location: "Detroit, MI",
+    website: "https://teamneon.io",
+    name: "Neon",
     type: ORG_TYPES.STARTUP,
   },
   {
